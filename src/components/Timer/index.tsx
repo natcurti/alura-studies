47c897b27fe9a082adcb9b20e7fde3b9be764b81
natcurti <naticurti01@gmail.com>
2024-02-23 14:29:45 -0300

    Create function countdown

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -18,13 +18,22 @@ const Timer = ({ selected }: IProps) => {
     }
   }, [selected]);
 
+  const countDown = (time: number = 0) => {
+    setTimeout(() => {
+      if (time > 0) {
+        setTime(time - 1);
+        return countDown(time - 1);
+      }
+    }, 1000);
+  };
+
   return (
     <div className={style.timer}>
       <p className={style.title}>Escolha um card e inicie o cronômetro</p>
       <div className={style.clockWrapper}>
         <Clock time={time} />
       </div>
-      <Button onClick={() => console.log("Começou")}>Começar</Button>
+      <Button onClick={() => countDown(time)}>Começar</Button>
     </div>
   );
 };
